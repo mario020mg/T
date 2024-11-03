d37088b8c440932eb43c6a69fe0813ffb1d1fbe1
Mario Manzanares Galvany <mario020mg@gmail.com>
2024-11-03 15:55:02 +0100

    aaa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2539955909"
+    serviceWorkerVersion: "1428271452"
   }
 });
