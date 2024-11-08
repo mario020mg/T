50c04c9cec6f5298bd64c9d2ef206de553cd1dd5
Mario Manzanares Galvany <58348457+mario020mg@users.noreply.github.com>
2024-11-08 20:07:26 +0100

    Photo Fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3253237463"
+    serviceWorkerVersion: "3916732872"
   }
 });
